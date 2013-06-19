dfb91dec664d1174c6e9f38ed1a0f72163c7bda7
tdway <toddway@gmail.com>
2013-06-18 20:11:56 -0500

    working on adding passport auth

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 var appAccounts = require('../data/store.js').appAccounts;
 var twilioSession = require('../controllers/session');
-var _           = require('underscore');
-var request     = require('request');
+var _ = require('underscore');
+var request = require('request');
+var passport = require('passport');
 
 //fake a user login, just uses phone number - no password
 exports.fakeLogin = function(req, res) {
@@ -76,4 +77,47 @@ exports.home = function(req, res){
     }
     
   });
+};
+
+exports.getlogin = function(req, res) {
+  res.render('login', { user: req.user, message: req.session.messages });
+};
+
+
+// POST /login
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+//
+//   curl -v -d "username=bob&password=secret" http://127.0.0.1:3000/login
+//   
+/***** This version has a problem with flash messages
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+  });
+*/
+  
+// POST /login
+//   This is an alternative implementation that uses a custom callback to
+//   acheive the same functionality.
+exports.postlogin = function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+    if (err) { return next(err) }
+    if (!user) {
+      req.session.messages =  [info.message];
+      return res.redirect('/postlogin1')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
+};
+
+exports.logout = function(req, res) {
+  req.logout();
+  res.redirect('/');
 };
